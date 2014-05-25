53e4ff1afc78a04a28638de89108d3424d4bc51f
sakren <sakren@gmail.com>
2014-05-25 18:58:28 +0200

    standalone files are build with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,35 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('compile-source', function() {
 	gulp.src('./src/**/*.coffee')
 		.pipe(coffee().on('error', gutil.log))
 		.pipe(gulp.dest('./lib/'));
 });
+
+gulp.task('compile-standalone-develop', function() {
+	gulp.src('./src/Build.coffee', {read: false})
+		.pipe(browserify({
+			transform: ['coffeeify'],
+			extensions: ['.coffee']
+		}))
+		.pipe(rename('http.js'))
+		.pipe(gulp.dest('./build/'));
+});
+
+gulp.task('compile-standalone-minify', function() {
+	gulp.src('./src/Build.coffee', {read: false})
+		.pipe(browserify({
+			transform: ['coffeeify'],
+			extensions: ['.coffee']
+		}))
+		.pipe(uglify())
+		.pipe(rename('http.min.js'))
+		.pipe(gulp.dest('./build/'));
+});
+
+gulp.task('compile-standalone', ['compile-standalone-develop', 'compile-standalone-minify'], function() {});
